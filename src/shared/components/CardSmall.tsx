7272c8309ec384ae6dadaf5b16d722f57d5c3bd1
Igor Queiroz <36078226+igorqsilvestre@users.noreply.github.com>
2025-12-30 12:02:53 -0300

    ajustando ainda o a página index e os cards

--- src/shared/components/CardSmall.tsx
+++ src/shared/components/CardSmall.tsx
@@ -1,6 +1,5 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { useEffect, useRef, useState } from "react";
-import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { theme } from "../themes/theme";
 
 const ITEM_HEIGHT = 30;
@@ -13,24 +12,9 @@ interface IPropsCardSmall {
     tipo?: 'default' | 'exercise';
 }
 export const CardSmall = ({backgroundColor, title, tempoOuQuantidade, tipo = 'default'}: IPropsCardSmall) => {
+    
 
-  const [quantidadeOuTempo, setQuantidadeOuTempo] = useState<string>();
-  const inputRef = useRef<TextInput>(null);
 
-  useEffect(() => {
-    if(tempoOuQuantidade){
-      setQuantidadeOuTempo(tempoOuQuantidade.toString());
-    }
-  }, [tempoOuQuantidade])
-
-  const listHeight =
-    tipo === 'exercise'
-      ? Math.min(tempoOuQuantidade, MAX_ITEMS_VISIBLE) * ITEM_HEIGHT
-      : undefined;
-
-    const finalizarEdicaoQuantidadeOuTempo = () => {
-      console.log('estou aqui');
-    }
 
     return (
         <View style={{ backgroundColor, ...styles.card}}>
@@ -38,65 +22,56 @@ export const CardSmall = ({backgroundColor, title, tempoOuQuantidade, tipo = 'de
             <MaterialIcons size={28} name="restart-alt"  />
           </TouchableOpacity>
           <View>
-            <View style={{ paddingBottom: 20, alignItems: 'center' }}>
+            <View style={{gap:8, paddingBottom: tipo === 'exercise' ? 0 : 20, alignItems: 'center' }}>
               <Text style={styles.cardTitle}>{ title }</Text>
-              <TextInput
-              ref={inputRef}
-              keyboardType="numeric"
-              value={quantidadeOuTempo}
-              onChangeText={setQuantidadeOuTempo}
-              style={styles.cardSubTitle}
-              onBlur={finalizarEdicaoQuantidadeOuTempo}
-              />
+              <Text>{ tempoOuQuantidade }</Text>
 
-              <TouchableOpacity onPress={() => inputRef.current?.focus()}>
+              <TouchableOpacity>
                 <MaterialIcons size={23} name="edit"  />
               </TouchableOpacity>
 
              {tipo === 'exercise' && (
               <View
-                style={{
-                  height: listHeight,
-                  width: '100%'
-                }}
+                style={styles.containerList}
               >
+                <TouchableOpacity>
+                  <MaterialIcons style={{position: 'absolute', right: 2,alignSelf: 'flex-end'}} size={23} name="edit"  />
+                </TouchableOpacity>
                 <FlatList 
                 data={Array.from(
-                  { length: Number(quantidadeOuTempo) },
+                  { length: tempoOuQuantidade },
                   (_, index) => ({ id: index + 1 })
                 )}
                 keyExtractor={(item) => item.id.toString()}
                 numColumns={2}
-                columnWrapperStyle={{ justifyContent: 'space-between' }}
-                contentContainerStyle={{  
-                  rowGap: 6,
-                  paddingHorizontal: 10
+                columnWrapperStyle={{ columnGap: 20, justifyContent: 'center' }}
+                contentContainerStyle={{
+                  rowGap: 10,
+                  paddingVertical: 8,
                 }}
+                style={{
+                   maxHeight: ITEM_HEIGHT * MAX_ITEMS_VISIBLE
+                }}
+                showsVerticalScrollIndicator={tempoOuQuantidade > MAX_ITEMS_VISIBLE}
+                scrollEnabled={tempoOuQuantidade > MAX_ITEMS_VISIBLE}
                 renderItem={({ item }) => (
-                   <View style={{ width: '48%' }}>
+                   <View>
                       <Text style={{textAlign: 'center', ...styles.cardSubTitle}}>Exercício {item.id}º</Text>
                     
                       <View style={styles.contentExercicio}>
-                        <View style={{flexDirection: 'row', gap: 4}}>
                           <Text style={styles.cardSubTitle}>Time</Text>
                           <Text style={styles.cardSubTitle}>45</Text>
                           <TouchableOpacity>
                             <MaterialIcons size={23} name="edit"  />
                           </TouchableOpacity>
-                        </View>
-                          
-                        <View style={{flexDirection: 'row', gap: 4}}>
                           <Text style={styles.cardSubTitle}>Des</Text>
                           <Text style={styles.cardSubTitle}>15</Text>
                           <TouchableOpacity>
                             <MaterialIcons size={23} name="edit"  />
                           </TouchableOpacity>
-                        </View>
                       </View>
                     </View>
                 )}
-                showsVerticalScrollIndicator={Number(quantidadeOuTempo) > MAX_ITEMS_VISIBLE}
-                scrollEnabled={Number(quantidadeOuTempo) > MAX_ITEMS_VISIBLE}
                 >
                 </FlatList>
               </View>
@@ -125,12 +100,27 @@ const styles = StyleSheet.create({
   cardIconRefresh: {
    alignSelf: 'flex-end'
   },
-  contentExercicio: {
+  containerList: {
+    width: '100%',
+    borderTopColor: '#537896',
+    borderTopWidth: 1,
+    paddingTop: 4,
+    paddingBottom: 20,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 1
+  },
+
+   contentExercicio: {
+    gap: 2,
+    paddingHorizontal: 2,
+    paddingVertical: 1,
     flexDirection: 'row',
-    justifyContent: 'space-around',
     borderStyle: "solid",
     borderWidth: 1,
     borderRadius: 4,
- 
-  }
+  },
 });
