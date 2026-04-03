c7dd657e9c69eb1cfc12b23c2d4caf092dcc4f51
Igor Queiroz <igorczz.10@gmail.com>
2026-04-03 10:15:31 -0300

    importação estava errada do treinoStorage

--- src/shared/services/treinoStorage.ts
+++ src/shared/services/treinoStorage.ts
@@ -1,6 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Treino } from '../types/Treino';
-
+import { Treino } from '../types/treino';
 
 const KEY = '@treinos';
 
