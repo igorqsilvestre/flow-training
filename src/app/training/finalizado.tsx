80c5b4be83383a3adb765a207377637ff0d16587
Igor Queiroz <36078226+igorqsilvestre@users.noreply.github.com>
2026-05-04 10:04:54 -0300

    adicionado som para quando completa o treino e desenvolvendo a tela de finalizando

--- src/app/training/finalizado.tsx
+++ src/app/training/finalizado.tsx
@@ -1,15 +1,68 @@
-import { Text, View } from "react-native";
+import { theme } from '@/src/shared/themes/theme';
+import { playSound } from '@/src/shared/utils/audio';
+import { MaterialIcons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Finalizado() {
+
+    useEffect(() => {
+        playSound(require('@/assets/sounds/sucess.mp3'));
+    },[])
+
     return (
-        <View style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center'
-        }}>
-            <Text style={{ color: 'white' }}>
-                Treino terminado com sucesso!
+        <View style={styles.container}>
+            <View style={{flexDirection: 'row',gap: 10}}>
+                <MaterialIcons size={50} name="celebration"  color={theme.colors.white}/>
+                <MaterialIcons size={50} name="celebration"  color={theme.colors.white}/>
+                <MaterialIcons size={50} name="celebration"  color={theme.colors.white}/>
+                <MaterialIcons size={50} name="celebration"  color={theme.colors.white}/>
+            </View>
+            <Text style={styles.texto}>
+                Parabéns
             </Text>
+            <Text style={styles.texto}>
+                Treino realizado com sucesso
+            </Text>
+            <View style={styles.footer}>
+                <TouchableOpacity style={styles.footerBotao} onPress={() => router.push('/(tabs)')}>
+                    <Text style={styles.texto}>Voltar a home</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.footerBotao} onPress={() => router.push('/(tabs)/progresso')} >
+                    <Text style={styles.texto}>Ver progresso</Text>
+                </TouchableOpacity>
+            </View>
         </View> 
     )
 }
+
+const styles = StyleSheet.create({
+ container: {
+    backgroundColor: theme.colors.secundary,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 30
+ },
+
+ texto: {
+    color: theme.colors.white,
+    fontSize: theme.fonts.sizes.medium,
+    fontFamily: theme.fonts.family.medium
+ },
+
+ footer: {
+    flexDirection: 'row', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10
+ },
+
+ footerBotao: {
+    backgroundColor: '#5e5e5e',
+    borderRadius: 10,
+    padding: 10
+ }
+
+})
